34c3ded9ff90545d275b1acaf577838fd469c492
Oscar Swanros <swanros@gmail.com>
2015-01-24 13:03:21 -0600

    Change package name from routes to router, create base handler to make sure it always output json, move mux.Router logic to GetRoutes() method

---
+++ router/index.go
@@ -0,0 +1,16 @@
+package router
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func Index(res http.ResponseWriter, req *http.Request) {
+	message := map[string]string{"message": "Hello world!"}
+	json, err := json.Marshal(message)
+	if err != nil {
+		panic(err)
+	}
+
+	res.Write(json)
+}
