0e17fcd4892f31e9cca4da315fe088cd5d5683c0
Oscar Swanros <swanros@gmail.com>
2015-01-24 16:40:55 -0600

    Convenience methods for Page model, update structure

--- model/page.go
+++ model/page.go
@@ -1,10 +1,26 @@
 package model
 
-import "time"
+import (
+	"time"
+)
 
 type Page struct {
+	Id						int64
 	Url						string
 	LastCheckedAt time.Time
 	CreatedAt			time.Time
 	UpdatedAt			time.Time
+	HtmlString		string
+}
+
+func PagesToCheck() []*Page {
+	Db.LogMode(false)
+
+	var pages []*Page
+	Db.Where("last_checked_at < ?", time.Now().Add(-time.Millisecond * 1500)).Find(&pages)
+	return pages
+}
+
+func (p Page) Save() {
+	Db.Save(&p)
 }
