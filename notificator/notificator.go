c0cb15c7ac59c2d3142d524a51b1310e3d5fd5ae
Antonio Chavez <cavjzz@gmail.com>
2015-01-25 11:45:59 -0600

    Update notificator to only trigger if the environment is production

--- notificator/notificator.go
+++ notificator/notificator.go
@@ -1,26 +1,31 @@
 package notificator
 
 import (
-	"log"
 	"github.com/gophergala/not_golang_experts/model"
 	"github.com/mailgun/mailgun-go"
+	"log"
+	"os"
 )
 
 func SendPageUpdatedNotification(u *model.User, url string) {
-	go func() {
-		mg := mailgun.NewMailgun("nts.mailgun.org", "key-6muwgm3md06odh43loir2bqoa4dws086", "")
+	gopher_env := os.Getenv("GOPHER_ENV")
 
-		m := mg.NewMessage(
-				"Oscar Swanros <notif@gostalker.com>",        // From
-				"Update!",                    // Subject
-				"The page " + url + " has been updated. Check it out!",                         // Plain-text body
-				"Oscar Swanros <" + u.Email + ">",        // Recipients (vararg list)
-		)
+	if gopher_env == "production" {
+		go func() {
+			mg := mailgun.NewMailgun("nts.mailgun.org", "key-6muwgm3md06odh43loir2bqoa4dws086", "")
 
-		_, _, err := mg.Send(m)
+			m := mg.NewMessage(
+				"Oscar Swanros <notif@gostalker.com>", // From
+				"Update!", // Subject
+				"The page "+url+" has been updated. Check it out!", // Plain-text body
+				"Oscar Swanros <"+u.Email+">",                      // Recipients (vararg list)
+			)
 
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
+			_, _, err := mg.Send(m)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+		}()
+	}
 }
