9e7b721888ee63569b6b360a3e26d8b28f55a784
Antonio Chavez <cavjzz@gmail.com>
2015-01-25 17:13:48 -0600

    Add sign up and sign in modals functionality

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,107 @@
-$(function() {
-  console.log(Backbone)
-});
+!function($, Backbone, _) {
+  $.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+      if (o[this.name] !== undefined) {
+        if (!o[this.name].push) {
+          o[this.name] = [o[this.name]];
+        }
+        o[this.name].push(this.value || '');
+      } else {
+        o[this.name] = this.value || '';
+      }
+    });
+    return o;
+  };
+
+  var App = {
+    Views: {},
+    Models: {},
+    initialize: function() {
+      App.router = new App.Router();
+      Backbone.history.start({pushState: true, hashChange: false});
+    }
+  };
+
+  App.Router = Backbone.Router.extend({
+    routes: {
+      ""          : "index",
+      "dashboard" : "dashboard"
+    },
+    index: function() {
+      App.indexView = new App.Views.Index();
+    },
+    dashboard: function() {
+      App.indexView = new App.Views.Dashboard();
+    }
+  });
+
+  App.Views.Index = Backbone.View.extend({
+    el: '.container-fluid',
+
+    events: {
+      "click .js-open-sign-in-modal" : "openSignInModal",
+      "click .js-open-sign-up-modal" : "openSignUpModal"
+    },
+
+    signInModal: this.$('#signInModal'),
+    signUpModal: this.$('#signUpModal'),
+
+    initialize: function() {
+      App.signInView = new App.Views.SessionModal({ el: "#signInModal" });
+      App.signUpView = new App.Views.SessionModal({ el: "#signUpModal" });
+    },
+
+    openSignInModal: function(e) {
+      this.signInModal.modal('show');
+    },
+
+    openSignUpModal: function(e) {
+      this.signUpModal.modal('show');
+    }
+  });
+
+  App.Views.SessionModal = Backbone.View.extend({
+    events: {
+      "submit form" : "sendForm"
+    },
+
+    initialize: function() {
+      this.errorContainer = this.$el.find('.alert.alert-danger');
+    },
+
+    sendForm: function(e) {
+      e.preventDefault();
+      this.sendData(this.$el.find('form'));
+      return false;
+    },
+
+    sendData: function($form) {
+      var data    = $form.serializeObject()
+        , request = $.ajax({ url: $form.attr('action'), type: "POST", data: JSON.stringify({ user: data })})
+        , self    = this
+
+      request.success(function(response) {
+        localStorage.setItem("authToken", response.token);
+        document.location.href = '/dashboard';
+      });
+
+      request.error(function(response) {
+        self.errorContainer.html(response.responseJSON.error);
+        self.errorContainer.show();
+      });
+    }
+  });
+
+  App.Views.Dashboard = Backbone.View.extend({
+    el: "body",
+    initialize: function() {
+      console.log(this.$el);
+    }
+  });
+
+  $(document).ready(function() {
+    App.initialize();
+  });
+}(jQuery, Backbone, _);
